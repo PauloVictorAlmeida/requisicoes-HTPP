e31cd2df575c2bd3400b96b07e0465bb5f031314
PauloVictorAlmeida <paulovictor.a.g4@gmail.com>
2024-10-22 23:53:13 -0300

    aprendendo API

---
+++ index.js
@@ -0,0 +1,13 @@
+cep.onkeyup = () =>{
+   if(cep.value.length = 8){
+    fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+        .then(resposta=> resposta.json())
+        .then(resposta2=>{
+            //console.log(resposta2); deu certo, deu para visualizar no Devtools
+            rua.value = resposta2.logradouro;
+            bairro.value = resposta2.bairro;
+            cidade.value = resposta2.localidade;
+            estado.value = resposta2.uf;
+        })
+   }
+}
